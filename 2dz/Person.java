e5467dd8e333b8d78e1877b1ea1054c28179053e
Коровенко Татьяна <tom.ya2012@yandex.ru>
2017-08-28 14:38:16 +0300

    Update Person.java
    Homework 2
    2)  Реализовать методы Person

--- 2dz/Person.java
+++ 2dz/Person.java
@@ -1,1 +1,47 @@
+public class Person {
+    private final boolean man;
+    private final String name;
+    private Person spouse;
 
+    public Person(boolean man, String name) {
+        this.man = man;
+        this.name = name;
+    }
+    public boolean marry(Person person) {
+        boolean flag = false;
+        if (this.man!=person.man){
+            if (this.spouse == null) {
+                this.spouse = person;
+                if (person.spouse == null) {
+                    person.spouse = this;
+                } else {
+                    flag = true;
+                    person.spouse.spouse = null;
+                    person.spouse = this;
+                }
+
+
+            }
+            else{
+                flag = true;
+                this.spouse.spouse = null;
+                this.spouse = person;
+                if (person.spouse == null) {
+                    person.spouse = this;
+                } else {
+                    person.spouse.spouse = null;
+                    person.spouse = this;
+                }
+            }
+        }
+        return (this.man!=person.man && flag);
+    }
+    public boolean divorce() {
+        boolean flag = false;
+        if (this.spouse != null){
+            this.spouse = null;
+            flag = true;
+        }
+        return flag;
+    }
+}
