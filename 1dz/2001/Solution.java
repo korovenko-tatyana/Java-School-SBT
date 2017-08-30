a94c099c86d374f1ae08e952ce7dfed1e053da98
Коровенко Татьяна <tom.ya2012@yandex.ru>
2017-08-30 21:13:32 +0300

    Update Solution.java

--- 1dz/2001/Solution.java
+++ 1dz/2001/Solution.java
@@ -1,1 +1,15 @@
+//2001. Ветвления, циклы. А+В
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a+b);
+    }
+}
 
