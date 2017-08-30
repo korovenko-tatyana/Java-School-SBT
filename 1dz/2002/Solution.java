734f1634ca1366a9ed1a1251268c6c9be935cb94
Коровенко Татьяна <tom.ya2012@yandex.ru>
2017-08-30 21:14:13 +0300

    Create Solution.java

---
+++ 1dz/2002/Solution.java
@@ -0,0 +1,18 @@
+//2002. Ветвления, циклы. Сумма чисел
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < count; i++) {
+            sum += scanner.nextInt();
+        }
+
+        System.out.println(sum);
+    }
+}
